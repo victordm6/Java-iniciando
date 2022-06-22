1542bfae697b357686521d38b90e27a51ce347f7
victordm6 <victor.dm6@hotmail.com>
2022-06-21 21:05:06 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    public static void soma(double numero1, double numeor2){
+        double produto = numero1 + numeor2;
+        System.out.println("Soma:" + numero1 + " + " + numeor2 + " = " + produto);
+
+    }
+}
