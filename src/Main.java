3a8c7dc4784bb450873d55da36e139dda74f2727
victordm6 <victor.dm6@hotmail.com>
2022-06-22 21:51:18 -0300

    :sparkles: feat: saida para mensagem,calculadora e emprestimo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Calculadora.soma(2, 3);
+
+        System.out.println("\nCalculadora:");
+        
+        Calculadora.soma(2, 241);
+        Calculadora.subtracao(120, 33);
+        Calculadora.multiplica(8571.43, 2);
+        Calculadora.divisao(2, 10);
+
+        System.out.print("\nMensagem: \n");
+
+        Mensagem.mensagemDia(8);
+        Mensagem.mensagemDia(14);
+        Mensagem.mensagemDia(22);
+        Mensagem.mensagemDia(25);
+
+        System.out.print("\nEmprestismo: \n");
+
+        Emprestimo.banco(10, 10000);
+        
     }
 }
