dde0b00524c03498a805cca4fa65db3d2a06b101
victordm6 <victor.dm6@hotmail.com>
2022-06-22 21:53:36 -0300

    :sparkles: feat: operações soma, subtração, multiplicação e divisão

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,7 +1,25 @@
 public class Calculadora {
-    public static void soma(double numero1, double numeor2){
-        double produto = numero1 + numeor2;
-        System.out.println("Soma:" + numero1 + " + " + numeor2 + " = " + produto);
+
+    public static void soma(double numero1, double numero2){
+        double produto = numero1 + numero2;
+        System.out.println("Soma: " + numero1 + " + " + numero2 + " = " + produto);
+    }
+
+    public static void subtracao(double numero1, double numero2) {
+        double produto = numero1 - numero2;
+        System.out.println("Subtrção: " + numero1 + " - " + numero2 + " = " + produto);
+        
+    }
+    
+    public static void multiplica(double numero1, double numero2){
+        double produto = numero1 * numero2;
+        System.out.printf("Multiplicação: " + numero1 + " x " + numero2 + " = %.2f", produto);
 
     }
+    public static void divisao(double numero1, double numero2){
+        double produto = numero1 / numero2;
+        System.out.println("\nDivisão: " + numero1 + " / " + numero2 + " = " + produto);
+
+    }
+
 }
